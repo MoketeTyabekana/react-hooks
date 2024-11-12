330290866d2ce17e20c85ee33aec790ba8ccd3e6
Mokete <ematyabekana@gmail.com>
2024-11-12 19:43:19 +0200

    Declare the the first state and of the our function and also initialised the function setCount that we'll use to update our state. Furthermore, used the onClick method ofn the button, and created a button function to updated the state when the button is clicked.

--- react-useState/src/App.jsx
+++ react-useState/src/App.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import React,{useState} from 'react'
 
 const App = () => { 
+ const [count,setCount]=useState(0);
+
+function IncreaseCount(){
+  setCount(count+1);
+}
+
   return (
     <div>
-     
-     <h1>Counter:</h1>
-     <button>Increase</button>
+     <h1>Counter:{count}</h1>
+     <button onClick={IncreaseCount}>Increase</button>
     </div>
   )
 }
