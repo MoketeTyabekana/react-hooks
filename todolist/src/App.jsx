f9c2f21ff910ef82bfe24c2d0e5e434013440ace
Mokete <emtyabekana@gmail.com>
2025-02-02 20:12:04 +0200

    defined the state hooks for the input and list items.

--- todolist/src/App.jsx
+++ todolist/src/App.jsx
@@ -5,6 +5,8 @@ import { MdPlaylistAdd } from "react-icons/md";
 
 function App() {
 
+  const [todos, setTodos] = useState([]);
+  const [input, setInput] = useState('');
 
   return (
     <div className='w-full h-screen  bg-gray-100'> 
@@ -14,7 +16,7 @@ function App() {
       <div className='w-4/5 mx-auto mt-4 bg-blue-500 p-4 rounded-lg shadow-lg grid grid-row gap-2 justify-items-start'>
      
         <input type="text" name='item' placeholder='Enter A New List Item' className='bg-white w-full p-3 rounded outline-none' />
-        <button className='bg-blue-900 w-1/3 p-3 rounded text-white  font-bold hover:bg-blue-800 inline-flex items-center justify-center' >
+        <button className='bg-blue-900 w-1/3 p-2 rounded text-white  font-bold hover:bg-blue-800 inline-flex items-center justify-center' >
         <span className='pr-3'><MdPlaylistAdd size="2em" /></span> Add Item 
         </button>
         <div className='w-full bg-white p-4 rounded shadow-lg gap-2'>
