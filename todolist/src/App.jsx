5f107a20ad6d3931a83da20ef16554c3cee55615
Mokete <emtyabekana@gmail.com>
2025-02-02 20:03:26 +0200

    install react icons and added the Add To List icon, changed the list header font size and added a horizontal line below the heading.

--- todolist/src/App.jsx
+++ todolist/src/App.jsx
@@ -1,6 +1,7 @@
 // Code: App component
 import './App.css';
 import {useState}  from 'react';
+import { MdPlaylistAdd } from "react-icons/md";
 
 function App() {
 
@@ -13,11 +14,12 @@ function App() {
       <div className='w-4/5 mx-auto mt-4 bg-blue-500 p-4 rounded-lg shadow-lg grid grid-row gap-2 justify-items-start'>
      
         <input type="text" name='item' placeholder='Enter A New List Item' className='bg-white w-full p-3 rounded outline-none' />
-        <button className='bg-blue-900 w-full p-3 rounded text-white font-bold hover:bg-blue-800 ' >
-          Add Item
+        <button className='bg-blue-900 w-1/3 p-3 rounded text-white  font-bold hover:bg-blue-800 inline-flex items-center justify-center' >
+        <span className='pr-3'><MdPlaylistAdd size="2em" /></span> Add Item 
         </button>
         <div className='w-full bg-white p-4 rounded shadow-lg gap-2'>
-           <h2 className='text-center font-bold'>Your List</h2>
+           <h2 className='text-center font-bold text-lg'>Your List</h2>
+           <hr  />
            <ul className='w-full gap-2'> 
 
            </ul>
