203a4f61226b4c8ffa59543c11f0a17ab04dcc30
Mokete <emtyabekana@gmail.com>
2025-02-10 18:55:16 +0200

    Created a button to handle the change function effect of the theme.

--- darkmodetoggle/src/App.jsx
+++ darkmodetoggle/src/App.jsx
@@ -1,12 +1,13 @@
-import './App.css'
+import './App.css';
+import { createContext, useContext, useState, useEffect } from 'react';
 
 function App() {
 
 
   return (
-    <>
-    <h1>Mokete</h1>
-    </>
+    <div className='flex justify-center items-center h-screen'>
+      <button className='px-8 py-2 bg-blue-800 hover:bg-blue-400 shadow-xl text-white rounded-full uppercase'>Toggle Theme</button>
+    </div>
   )
 }
 
