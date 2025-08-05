5a576463943b658bafd697dc2e58e258165a5db0
Mokete <ematyabekana@gmail.com>
2025-08-05 21:29:03 +0200

    Created a basic form that uses the useState to manage the state of the of the input.

--- react-useState/src/App.jsx
+++ react-useState/src/App.jsx
@@ -1,23 +1,51 @@
-import React,{useState} from 'react';
+import React, { useState } from "react";
 
-const App = () => { 
- const [count,setCount]=useState(0);
+const App = () => {
+  const [count, setCount] = useState(0);
+  const [name, setName] = useState("");
 
-function IncreaseCount(){
-  setCount(count+1);
-}
+  function IncreaseCount() {
+    setCount((prev) => prev + 1);
+  }
 
-function DecreaseCount(){
-  setCount(count-1);
-}
+  function DecreaseCount() {
+    setCount((prev) => prev - 1);
+  }
 
   return (
-    <div>
-     <h1>Counter:{count}</h1>
-     <button onClick={IncreaseCount}>Increase</button>
-     <button onClick={DecreaseCount}>Increase</button>
+    <div 
+         style={{
+       textAlign: "center",
+       display: "flex",
+       flexDirection: "column",
+       alignItems: "center",
+       gap: "10px",
+     }
+   }
+    >
+      <div
+        style={{
+          textAlign: "center",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          gap: "10px",
+        }
+      }
+      >
+        <h1>Counter:{count}</h1>
+        <button onClick={IncreaseCount}>Increase</button>
+        <button onClick={DecreaseCount}>Increase</button>
+      </div>
+
+      <form action="">
+        <label htmlFor="">Name: </label>
+        <input type="text" name="name" id="name" value={name} onChange={(e)=> setName(e.target.value)} placeholder="Enter Your Name"/>
+        <p>hello , {name}</p>
+
+      </form>
     </div>
-  )
-}
+  );
+};
 
 export default App;
